a24747a561a5cd390138317bb2a43e834249d3a4
Richard Appleby <appleby_richard@msn.com>
2014-11-30 04:03:06 +0000

    after many problems, pushing a new prototype

--- popguessr.js
+++ popguessr.js
@@ -8,18 +8,20 @@ require(["esri/Color",
   "esri/map",
   "esri/layers/ArcGISDynamicMapServiceLayer",
   "esri/graphic",
-  "esri/tasks/Geoprocessor",
   "esri/tasks/FeatureSet",
-  "esri/toolbars/draw",
   "esri/symbols/SimpleLineSymbol",
   "esri/symbols/SimpleFillSymbol",
   "esri/urlUtils",
   "esri/geometry/Polygon",
   "esri/geometry/Point",
   "esri/layers/FeatureLayer",
-  "esri/tasks/query"
+  "esri/tasks/query",
+  "esri/tasks/QueryTask",
+  "esri/layers/GraphicsLayer"
   ],
-function(Color, string, registry, esriConfig, Map, ArcGISDynamicMapServiceLayer, Graphic, Geoprocessor, FeatureSet, Draw, SimpleLineSymbol, SimpleFillSymbol, urlUtils, Polygon, Point, FeatureLayer, Query){
+function(Color, string, registry, esriConfig, Map, ArcGISDynamicMapServiceLayer,
+  Graphic, FeatureSet, SimpleLineSymbol, SimpleFillSymbol,
+  urlUtils, Polygon, Point, FeatureLayer, Query, QueryTask, GraphicsLayer){
 
   //Proxy Boilerplate
   urlUtils.addProxyRule({
@@ -30,87 +32,68 @@ function(Color, string, registry, esriConfig, Map, ArcGISDynamicMapServiceLayer,
   esriConfig.defaults.io.proxyUrl = "/proxy";
   esriConfig.defaults.io.alwaysUseProxy = false;
 
-  var map, gp, cities;
+  var map, regions;
+
+  var MAX_NUMBER_OF_GUESSES = 10;
+
+  function Game(max) {
+    this.maxNumberOfGuesses = max;
+    this.score = 0;
+    this.guesses = 0;
+  }
 
   // Create Map
   map = new Map("mapDiv", { 
-    basemap: "streets", 
+    basemap: "gray", 
     center: [5, 50],
-    zoom: 4
+    zoom: 9
   });
 
-  map.on("load", initApp);
+  map.on("load", init);
 
-  // Create GeoProcessor
-  gp = new Geoprocessor("http://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Demographics/ESRI_Population_World/GPServer/PopulationSummary");
-  gp.setOutputSpatialReference({wkid:102100});
-
-  function initApp(evtObj) {
-    var citiesLayer = new FeatureLayer("http://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/World_Cities/FeatureServer/0");
-
-    citiesLayer.on("load", function(e) {
-      getCities(citiesLayer, startGame);
-    });
+  function init() {
+    getRegions(startGame);
   }
 
-  function startGame() {
-    var index;
-
-    index = 0;
-    loop();
-
-    function loop() {
-      if (index < 10) {
-        pickCity();
-        setTimeout(loop, 2000);
-      }
-    }
-  }
-
-  function getCities(citiesLayer, callback) {
-    // Dirty hack - gets all cities, will do once at start of code
+  function getRegions(callback) {
+    // Dirty hack - gets all regions, will do once at start of code
     var query = new Query();
-    query.where = "CITY_NAME LIKE '%'";
-    citiesLayer.selectFeatures(query, FeatureLayer.SELECTION_NEW, function(features, method) {
-      cities = features;
+    var queryTask = new QueryTask("http://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/usa_cbsa/FeatureServer/0");
+
+    query.where = "NAME LIKE '%'";
+    query.num = 20;
+    query.outFields = ["NAME", "POP2012"];
+    query.returnGeometry = true;
+
+    queryTask.execute(query, function(args) {
+      regions = args.features;
       callback();
     });
   }
 
-  function pickCity() {
-    var numOfCities = cities.length;
-    var rand = Math.floor((Math.random() * numOfCities) + 1);
-    var city = cities[rand];
-    var p = new Point(city.geometry.x, city.geometry.y, {wkid:102100});
-    console.log(p);
-    map.centerAt(p);
+  function startGame() {
+    var g = new Game(MAX_NUMBER_OF_GUESSES);
+    pickRegion(function(population) {
+      console.log(population);
+    });
   }
 
-  function computeZonalStats(geometry) {
-    map.showZoomSlider();
+  function pickRegion(callback) {
+    var numOfRegions = regions.length;
+    var rand = Math.floor((Math.random() * numOfRegions));
+    var region = regions[rand];
+    var p = region.geometry.getCentroid();
+
+    map.centerAt(p);
+
     map.graphics.clear();
-    
-    var symbol = new SimpleFillSymbol("none", new SimpleLineSymbol("dashdot", new Color([255,0,0]), 2), new Color([255,255,0,0.25]));
-    var graphic = new Graphic(geometry,symbol);
+        
+    var symbol = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID, new SimpleLineSymbol("solid", new Color([255,0,0]), 4), new Color([255,255,0,0.25]));
+    var graphic = new Graphic(region.geometry,symbol);
 
     map.graphics.add(graphic);
 
-    var features= [];
-    features.push(graphic);
-
-    var featureSet = new FeatureSet();
-    featureSet.features = features;
-    
-    var params = { "inputPoly":featureSet };
-    gp.execute(params);
-  }
-
-  function displayResults(evtObj) {
-    var results = evtObj.results;
-    var content = string.substitute("<h4>The population in the user defined polygon is ${number:dojo.number.format}.</h4>",{number:results[0].value.features[0].attributes.SUM});
-
-    registry.byId("dialog1").setContent(content);
-    registry.byId("dialog1").show();
+    callback(region.attributes.POP2012);
   }
 
 });
